e5969795c0a8ef495bc0592a6875a68112520e95
Ivica <ivica-matic@outlook.com>
2023-05-17 23:31:12 +0100

    refactor: added comment to the abstract function

--- base_signing_middleware.py
+++ base_signing_middleware.py
@@ -8,4 +8,8 @@ class SigningMiddleware:
 
     @abstractmethod
     def sign_href(self, href) -> Tuple[str, bool]:
+        # Abstract method to be implemented by every signing middleware.
+        # This method should return the URL and a boolean indicating if the URL was signed or not.
+        # If the URL was not signed, the URL should be returned as is with the boolean set to False.
+        # If the URL was signed, hence changed, the new URL should be returned with the boolean set to True.
         pass
